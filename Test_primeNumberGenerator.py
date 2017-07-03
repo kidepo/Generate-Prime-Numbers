4c80ab642f2fd1b90570126ab3b21386bef3e9b7
kidepo <kiyingidenispaul@gmail.com>
2017-07-03 18:08:04 +0300

    Improved check for strings and testing result set with 0-50

--- Test_primeNumberGenerator.py
+++ Test_primeNumberGenerator.py
@@ -19,17 +19,20 @@ class TestPrimeGenerator(unittest.TestCase):
     def test_zero(self):
         """Testing if zero is correctly determined not to be prime."""
 
-        self.assertEqual(generatePrimeNumbers(
-            0), "Zero or One cannot be prime numbers.")
+        self.assertEqual(generatePrimeNumbers(0), "Zero or One cannot be prime numbers.")
+        
+    def test_fifty(self):
+        """Testing if prime numbers are correctly generated."""
+        
+        self.assertEqual(prime_numbers(50), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47])
 
     def test_one(self):
         """"Testing if one is correctly determined not to be prime."""
 
-        self.assertEqual(generatePrimeNumbers(
-            1), "Zero or One cannot be prime numbers.")
+        self.assertEqual(generatePrimeNumbers(1), "Zero or One cannot be prime numbers.")
 
-    def test_two(self):
-        """Testing if error returned if integer entered is 2."""
+    def test_string(self):
+        """Testing if error returned if a string is entered."""
 
         self.assertEqual(generatePrimeNumbers("String"), "Only integers are allowed.")
 
@@ -38,16 +41,10 @@ class TestPrimeGenerator(unittest.TestCase):
 
         self.assertEqual(generatePrimeNumbers([]), "Only integers are allowed.")
 
-    def test_invalid_type_string_set(self):
-        """Testing if error returned if input not integer."""
-
-        self.assertEqual(generatePrimeNumbers(set()), "Only integers are allowed.")
-
     def test_only_positive(self):
         """Testing if error returned if negative integers input."""
 
-        self.assertEqual(
-            generatePrimeNumbers(-1), "Not possible to generate prime numbers for integers less than 2.")
+        self.assertEqual(generatePrimeNumbers(-7), "Not possible to generate prime numbers for integers less than 2.")
 
 
 if __name__ == "__main__":
